e3ee8ba787186e66f03cd05273f54bae659cc04f
Griffin P Longtin <griflongtin@live.com>
2018-01-05 11:34:33 -0800

    add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var del = require('del');
+var jshint = require('gulp-jshint');
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-ui.js'])
