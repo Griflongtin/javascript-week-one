2f130454ce7df85f702f14e8ada0f5f86b8f0162
Griffin P Longtin <griflongtin@live.com>
2018-01-05 11:28:26 -0800

    add gulp-del the clean my build and tmp files for a new build

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var del = require('del');
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-ui.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
-
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ['./tmp/allConcat.js'] })
@@ -17,13 +17,15 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .pipe(gulp.dest('./build/js'));
 });
 
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
 
 
 
 
 
 
-
-gulp.task('build', function() {
+gulp.task('build', ['clean'], function() {
   gulp.start('jsBrowserify');
 });
